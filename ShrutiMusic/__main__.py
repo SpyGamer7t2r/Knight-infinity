9c724945c98b05e0ab340fcaee2e11b989455b49
༄ᶦᶰᵈ᭄Sᴘʏ✿Gᴀᴍᴇʀ࿐ <gamingwithmayuresh7t@gmail.com>
2025-06-05 15:21:56 +0530

    Update __main__.py

--- ShrutiMusic/__main__.py
+++ ShrutiMusic/__main__.py
@@ -47,4 +47,5 @@ async def init():
         await Aviax.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
         LOGGER("ShrutiMusic").error(
-            "Please turn on the videochat of your log group/channel.\n
+    "Please turn on the videochat of your log group/channel.\nStopping the bot..."
+)
