276092db5758c0f2ed0de93c66255bee85e25d67
༄ᶦᶰᵈ᭄Sᴘʏ✿Gᴀᴍᴇʀ࿐ <gamingwithmayuresh7t@gmail.com>
2025-06-05 14:29:11 +0530

    Update bot.py

--- ShrutiMusic/core/bot.py
+++ ShrutiMusic/core/bot.py
@@ -1,32 +1,56 @@
-async def start(self):
-    await super().start()
+import uvloop
 
-    me = await self.get_me()
-    self.id = me.id
-    self.name = me.first_name + " " + (me.last_name or "")
-    self.username = me.username
-    self.mention = me.mention
+uvloop.install()
 
-    try:
-        await self.send_message(
-            chat_id=config.LOG_GROUP_ID,
-            text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
-        )
-    except (errors.ChannelInvalid, errors.PeerIdInvalid):
-        LOGGER(__name__).error(
-            "Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel."
-        )
-        exit()
-    except Exception as ex:
-        LOGGER(__name__).error(
-            f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}."
-        )
-        exit()
+from pyrogram import Client, errors
+from pyrogram.enums import ChatMemberStatus, ParseMode
 
-    a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
-    if a.status != ChatMemberStatus.ADMINISTRATOR:
-        LOGGER(__name__).error(
-            "Please promote your bot as an admin in your log group/channel."
+import config
+from ..logging import LOGGER
+
+import uvloop:
+    uvloop.install():
+        LOGGER(name).info(f"Starting Bot...")
+        super().init(
+            name="ShrutiMusic",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            bot_token=config.BOT_TOKEN,
+            in_memory=True,
+            parse_mode=ParseMode.HTML,
+            max_concurrent_transmissions=7,
         )
-        exit()
-    LOGGER(__name__).info(f"Music Bot Started as {self.name}")
+
+    async def start(self):
+        await super().start()
+        self.id = self.me.id
+        self.name = self.me.first_name + " " + (self.me.last_name or "")
+        self.username = self.me.username
+        self.mention = self.me.mention
+
+        try:
+            await self.send_message(
+                chat_id=config.LOG_GROUP_ID,
+                text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
+            )
+        except (errors.ChannelInvalid, errors.PeerIdInvalid):
+            LOGGER(name).error(
+                "Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel."
+            )
+            exit()
+        except Exception as ex:
+            LOGGER(name).error(
+                f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).name}."
+            )
+            exit()
+
+        a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
+        if a.status != ChatMemberStatus.ADMINISTRATOR:
+            LOGGER(name).error(
+                "Please promote your bot as an admin in your log group/channel."
+            )
+            exit()
+        LOGGER(name).info(f"Music Bot Started as {self.name}")
+
+    async def stop(self):
+        await super().stop()
